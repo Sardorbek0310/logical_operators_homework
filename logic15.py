09966f121c91a142918ac986895753cb93d9a9af
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-08 05:44:52 -0400

    15 done

--- logic15.py
+++ logic15.py
@@ -1,3 +1,4 @@
+a=379
 def main(a):
     """
     Given a three-digit integer a,  check the following statement "All digits sum is odd".
@@ -6,4 +7,5 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    return (a//100+(a%100)//10+a%10)%2!=0
+print(main(a))
