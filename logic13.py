1cf899295325a6d7d3aa8b0408f2d178e575242c
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-08 05:37:50 -0400

    13 done

--- logic13.py
+++ logic13.py
@@ -1,3 +1,4 @@
+a=40
 def main(a):
     """
     Given a two-digit integer a,  check the following statement "All digits sum is even".
@@ -6,4 +7,5 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    return (a//10+a%10)%2!=1
+print(main(a))
